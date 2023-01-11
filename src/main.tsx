e15d7bfc3f065bd58bd387ed7f57718990812ce0
Romeo portable <romeo.malivert@ecole.ensicaen.fr>
2023-01-11 11:29:43 +0100

    fixed indentations

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import LoginPage from './LoginPage'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <LoginPage />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<LoginPage />
+	</React.StrictMode>,
 )
